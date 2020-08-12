2da9abbf867dfc83d0dbc74e90c22152b48e0706
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-08-12 08:58:19 +0200

    Add project files.

---
+++ S3_IPayable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S3_IPayable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
